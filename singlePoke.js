8949f4dc25071fa32ebe439fc15dc5d47882072d
Marvin Clerc <marvin.clerc@edu.devinci.fr>
2023-01-11 17:04:12 +0100

    drag and drop part 1

--- singlePoke.js
+++ singlePoke.js
@@ -1,5 +1,6 @@
-const dexContainer = document.getElementById("milieu")
-//const dexleftcont  = document.getElementById("droite")
+const dexContainer = document.getElementById("milieu");
+const dexleftcont  = document.getElementById("gauche");
+
 //const idpoke = url.searchParams.get("idpoke");
 async function getpokebyid() {
     try {
@@ -14,7 +15,7 @@ async function getpokebyid() {
                 `<div id="haut">
                         <h2>${data.name}</h2>
                 </div>
-                <div id="divImg">
+                <div id="divImg" ondragstart="dragStart(event)">
                     <img id="imgPoke" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png" alt="">
                 </div>
                 <div id="bas">
@@ -22,6 +23,45 @@ async function getpokebyid() {
                     <div></div>
                     <div></div>
                 </div> `;
+        dexleftcont.innerHTML = "";
+        dexleftcont.innerHTML +=
+            `
+            <div class="team_element"</div>
+            <div class="team_element"></div>
+            <div class="team_element"></div>
+            <div class="team_element"></div>
+            <div class="team_element"></div>
+            <div class="team_element"></div> 
+`;
+
+
+
+            var drag = document.getElementById("divImg");
+            drag.draggable = "true";
+            drag.addEventListener("dragstart", dragbeg);
+            var team = document.querySelectorAll(".team_element");
+            for(let element of team){
+                element.addEventListener("dragover", autoriserDrop);
+                element.addEventListener("drop", drop);
+            }
+
+
+
+        function dragbeg(event) {
+            event.dataTransfer.setData("team", event.target.id);
+        }
+        function autoriserDrop(event) {
+            event.preventDefault();
+            event.dataTransfer.dropEffect = "move";
+        }
+        function drop(event) {
+            event.preventDefault();
+            var datapoke = event.dataTransfer.getData("team");
+            event.target.appendChild(document.getElementById(datapoke));
+            console.log(datapoke)
+        }
+
+
 
     } catch (error) {
         dexContainer.innerHTML = `<p>${error.message}</p>`;
