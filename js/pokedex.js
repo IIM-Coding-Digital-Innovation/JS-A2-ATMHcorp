d6ea59415af5ba9655e7a4417f9bd500ec48fea1
Arthur Pouteau <arthur.pouteau92@gmail.com>
2023-01-12 18:41:46 +0100

    :sparkles: display all poke

--- js/pokedex.js
+++ js/pokedex.js
@@ -2,7 +2,8 @@ const prevBtn = document.getElementById('prev')
 const nextBtn = document.getElementById('next')
 const pokeWrapperDiv = document.getElementById('divWrapper') 
 let pokemons = []
-const baseUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=150"
+let pokemonsUrl = []
+const baseUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=50"
 let nextUrl = ""
 let prevUrl = ""
 
@@ -11,30 +12,51 @@ async function getAllPokeLinks(url) {
         const response = await fetch(`${url}`);
         const data = await response.json();
         pokemons = data.results;
+
         nextUrl = data.next
         prevUrl = data.previous
-        document.dispatchEvent(new Event('pokemonsFetched'));
+
+        pokemons.forEach( item =>{
+            pokemonsUrl.push(item.url)
+        })
+
+        pokemonsUrl.forEach(item =>{
+            getEachPoke(item)
+        })
+
+        console.log(pokemonsUrl)
     } catch (error) {
         console.error(error);
     }
 }
-document.addEventListener('pokemonsFetched', displayPokemons);
+async function getEachPoke(url) {
+    const response = await fetch(`${url}`);
+    const data = await response.json();
 
-function displayPokemons() {
-    pokeWrapperDiv.innerHTML = ""
-    pokemons.forEach( pokemon =>{
-        pokeWrapperDiv.innerHTML += `
-        <li>${pokemon.name}</li>`
-    })}
+    pokeWrapperDiv.innerHTML += `
+    <a href="singlePoke.html?id=${data.id}">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png">
+        <p>${data.name}</p>
+    </a>
+    `
+
+}
 
 getAllPokeLinks(baseUrl)
 
 prevBtn.addEventListener('click', function() {
     pokemons = []
+    pokemonsUrl = []
+    pokeWrapperDiv.innerHTML = ""
     getAllPokeLinks(prevUrl)
 });
 
 nextBtn.addEventListener('click', function() {
     pokemons = []
+    pokemonsUrl = []
+    pokeWrapperDiv.innerHTML = ""
     getAllPokeLinks(nextUrl)
 });
+
+
+
