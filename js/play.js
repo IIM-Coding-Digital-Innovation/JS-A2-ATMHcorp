28519a6764f892f78eec702703e719f34e1dd0d0
Thomas Fi <tom.filhol@gmail.com>
2023-01-13 01:59:54 +0100

    :sparkles: ajouts de fonctionnalités + corrections bugs

--- js/play.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('click', musicPlay);
-function musicPlay() {
-document.getElementById('audio').play();
-document.removeEventListener('click', musicPlay);}
--- js/start.js
+++ js/start.js
@@ -1,10 +1,19 @@
 const menu = document.getElementById('menu');
+document.addEventListener('click', musicPlay);
 
 document.onkeydown = function(event) {
     if (event.code === "Enter") {
+        document.removeEventListener('click', musicPlay);
         menu.style.visibility = "hidden";
         menu.style.pointerEvents = "none";
     }
 }
+
+function musicPlay() {
+    document.getElementById('audio').play();
+    document.removeEventListener('click', musicPlay);
+}
+
+
 
 
